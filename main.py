cebd5edcd58f1be3f8f9d082d2efb58346926c3b
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2021-11-22 18:55:34 +0100

    Project structure minor correction

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
+from app.missing_values import cca_func, mean_func, median_func
 
-from missing_values import cca_func, mean_func, median_func
+
+
 #Rappel: Pour lancer l'app "streamlit run main.py" à partir du répertoire courant
-
-#cca function
-
 
 st.title('AutoML')
 uploaded_file = st.file_uploader("Selectionner un dataset",type=['csv','xlsx'],accept_multiple_files=False)
@@ -21,10 +20,10 @@ if uploaded_file is not None:
         data = cca_func(data)
 
     if select == "Mean imputation":
-        mean_func(data)
+        data = mean_func(data)
 
     if select == "Median imputation":
-        median_func(data)
+        data = median_func(data)
 
 
 
